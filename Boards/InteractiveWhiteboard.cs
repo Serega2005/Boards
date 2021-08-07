b0ec1a4ee0d6c84ca83946744be5d815e61ba551
Serega <Schuganiserega@mail.ru>
2021-08-07 20:31:26 +0400

    Выполнена домашняя работа

--- Boards/InteractiveWhiteboard.cs
+++ Boards/InteractiveWhiteboard.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Boards
 {
+    //  Описание доскок
     class InteractiveWhiteboard : Board, IWritingAffiliation
     {
         public string WritingAffiliation { get; set; }
